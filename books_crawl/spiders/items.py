1d0c7e5c84db46e6c8e3cac68613cf85b4d1baab
hunkyguy <hunkyguy@163.com>
2013-11-22 14:53:36 +0800

    Update items.py

--- books_crawl/spiders/items.py
+++ books_crawl/spiders/items.py
@@ -6,7 +6,7 @@
 
 from scrapy.item import Item, Field
 
-class BooksDoubanScrapyItem(Item):
+class BooksScrapyItem(Item):
     # define the fields for your item here like:
     url = Field()
     name = Field()
